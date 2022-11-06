9913261e3b9e85f77ab7c0a17055174d1c37f679
Eunjeong Kim <ryomi.j78@gmail.com>
2022-11-06 15:54:13 +0900

    added some functions to prepare to play a game

--- script/app.js
+++ script/app.js
@@ -15,10 +15,12 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
 
 const player1EditBtn = document.getElementById("player1-edit-btn");
 const player2EditBtn = document.getElementById("player2-edit-btn");
 const cancelConfigBtn = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
 
 player1EditBtn.addEventListener("click", openPlayerConfig);
 player2EditBtn.addEventListener("click", openPlayerConfig);
@@ -27,3 +29,5 @@ cancelConfigBtn.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
