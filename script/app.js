9c3afd0c49177df145d851d0b2062d382abf7039
Eunjeong Kim <ryomi.j78@gmail.com>
2022-11-06 16:31:40 +0900

    added functions for selecting blocks, switching players, and showing notice

--- script/app.js
+++ script/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -16,11 +17,18 @@ const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const player1EditBtn = document.getElementById("player1-edit-btn");
 const player2EditBtn = document.getElementById("player2-edit-btn");
 const cancelConfigBtn = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
+
+// li를 선택하는 경우
+// const gameFields = document.querySelectorAll("#game-board li");
+
+// ol을 선택하는 경우
+const gameBoardElement = document.getElementById("game-board");
 
 player1EditBtn.addEventListener("click", openPlayerConfig);
 player2EditBtn.addEventListener("click", openPlayerConfig);
@@ -31,3 +39,11 @@ backdropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
+
+// li를 선택하는 경우
+// for (const gameField of gameFields) {
+//   gameField.addEventListener("click", selectGameField);
+// }
+
+// ol을 선택하는 경우
+gameBoardElement.addEventListener("click", selectGameField);
--- script/game.js
+++ script/game.js
@@ -1,9 +1,39 @@
 function startNewGame() {
-if(!players[0].name|| !players[1].name){
-    alert('Please set custom player names for both players!');
-    return
+  if (!players[0].name || !players[1].name) {
+    alert("Please set custom player names for both players!");
+    return;
+  }
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
 }
 
-    gameAreaElement.style.display = 'block';
+function switchPlayer() {
+  if (activePlayer == 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
 
+// li를 선택하는 경우
+// function selectGameField(event) {
+//   event.target.textContent = players[activePlayer].symbol;
+//   event.target.classList.add("disabled");
+
+//   switchPlayer()
+// }
+
+// ol을 선택하는 경우
+function selectGameField(event) {
+  if (event.target.tagName !== "LI") {
+    // ol의 li 이외 부분을 클릭하는 경우 배제
+    return;
+  }
+
+  event.target.textContent = players[activePlayer].symbol;
+  event.target.classList.add("disabled");
+
+  switchPlayer();
 }
